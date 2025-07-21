031ff46e21d5ca7c48cfc482cb5d374eddd44b5a
mkusaka <hinoshita1992@gmail.com>
2025-07-21 22:40:09 +0900

    refactor: remove unused code and modules
    Removed unused modules and their dependencies:
    - optimized_engine.rs (marked with #[allow(dead_code)])
    - mmap_reader.rs (only used by optimized_engine)
    - fast_json_scanner.rs (only used by optimized_engine)
    
    All tests still pass after removal.

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -2,10 +2,6 @@
 pub mod async_engine;
 pub mod engine;
 pub mod file_discovery;
-mod mmap_reader;
-mod fast_json_scanner;
-#[allow(dead_code)]
-mod optimized_engine;
 
 #[cfg(feature = "async")]
 pub use async_engine::{AsyncSearchEngine, AsyncSearchOptions, AsyncSearchResult};
