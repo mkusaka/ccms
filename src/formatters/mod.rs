383fc17228b4839dd2bb3a97b571ee802599bcf3
mkusaka <hinoshita1992@gmail.com>
2025-08-04 01:27:23 +0900

    fix: Apply rustfmt to fix CI formatting errors

--- src/formatters/mod.rs
+++ src/formatters/mod.rs
@@ -1,3 +1,5 @@
 pub mod tool_parser;
 
-pub use tool_parser::{parse_raw_json, parse_text_content, ParsedContent, ToolExecution, ThinkingBlock};
+pub use tool_parser::{
+    ParsedContent, ThinkingBlock, ToolExecution, parse_raw_json, parse_text_content,
+};
